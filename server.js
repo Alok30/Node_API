dd79539fb9622dedbe2249275f9d47081c1e53bb
Alok Dubey <alokje2013@gmail.com>
2019-01-08 16:57:07 +0530

    alok project

---
+++ server.js
@@ -0,0 +1,8 @@
+   const express=require("express");
+   const app=express();
+   app.get("/posts",(req,res)=>{
+       
+   })
+   app.listen(3001,function(){
+       console.log("Server is running 20");
+   });
